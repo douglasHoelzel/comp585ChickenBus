37fdced64c62af55cf33ef4c61c0926db854653f
Doug <douglas.hoelzel@gmail.com>
2018-02-02 12:23:23 -0500

    added map to MapPage.js

--- app/screens/MapPage.js
+++ app/screens/MapPage.js
@@ -7,12 +7,21 @@ import {
 } from 'react-native';
 import { List, ListItem } from 'react-native-elements';
 import { users } from '../config/data';
+import MapView from 'react-native-maps';
 
 class MapPage extends Component {
   render() {
     return (
         <View style={styles.container}>
-             <Text style={styles.text}>Map Goes Here</Text>
+            <MapView
+                style={styles.map}
+                initialRegion={{
+                  latitude: 37.78825,
+                  longitude: -122.4324,
+                  latitudeDelta: 0.0922,
+                  longitudeDelta: 0.0421,
+                }}
+              />
         </View>
     );
   }
@@ -20,10 +29,18 @@ class MapPage extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 6,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#17A2F1',
+},
+map:{
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    justifyContent: 'flex-end',
 },
 text: {
   justifyContent: 'center',
